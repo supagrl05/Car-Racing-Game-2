7a6e61a63c455be8f58bb5799e2d9feb0b59ec19
supagrl05 <73520340+supagrl05@users.noreply.github.com>
2021-01-09 17:22:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas, backGroundImage;
+var database;
+var gameState = 0;
+var playerCount;
+var form, player, game;
+
+function setup(){
+    canvas = createCanvas(400, 400);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+
+}
